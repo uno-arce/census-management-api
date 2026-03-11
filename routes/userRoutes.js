907e65d923cf50bd05e7931cdeb505bb3cd93bca
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-11 19:26:34 +0800

    Added user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,15 @@
-userRoutes.js
+const express = require('express')
+const router = express.Router()
+const { verify } = require('../auth')
+const userController = require('../controllers/userController.js')
+
+router.post('/register', userController.registerUser)
+router.post('/login', userController.loginUser)
+router.post('/logout', userController.logoutUser)
+router.get('/verify', verify, (req, res) => {
+	res.status(200).send({ message: 'User is authenticated'})
+})
+router.put('/change-password', verify, userController.changePassword)
+router.get('/get-history', verify, userController.getUserHistory)
+
+module.exports = router
