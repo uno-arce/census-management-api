6876ba608c9fcee7ba1b65f4b35c75a1f5fc0e12
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-11 19:26:12 +0800

    Added record routes

--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -1,1 +1,11 @@
-recordRoutes.js
+const express = require('express')
+const router = express.Router()
+const { verify } = require('../auth')
+const recordController = require('../controllers/recordController.js')
+
+router.get('/get-records', recordController.getRecords)
+router.post('/create-records', verify, recordController.createRecords)
+router.put('/update-records', verify, recordController.updateRecord)
+router.delete('/delete-records', verify, recordController.deleteRecords)
+
+module.exports = router
