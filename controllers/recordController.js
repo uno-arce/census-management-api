9812742f66e9b81aca3ff883da14735a4154e8b4
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-12 18:53:08 +0800

    Modified CRUD logic

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -4,19 +4,29 @@ module.exports.createRecords = async (req, res) => {
 	try {
         const userId = req.user.id
 
-        const recordsWithAuthor = req.body.records.map(record => ({
-            ...record,
-            authorId: userId,
-            birthDate: new Date(record.birthDate) 
-        }))
+        const recordsWithAuthor = req.body.records.map(record => {
+        	const { id, authorId, createdAt, ...safeBody } = record
+            return {
+            	...record,
+            	authorId: userId,
+            	birthDate: new Date(record.birthDate) 
+            }
+        })
 
         const records = await prisma.records.createManyAndReturn({
             data: recordsWithAuthor,
             skipDuplicates: true
         })
 
+        if(records.length === 0) {
+        	return res.status(400).send({
+        		message: 'No new records added. These residents are already recorded in the database',
+        		records: []
+        	})
+        }
+
         return res.status(200).send({ 
-        	message: 'Records successfully added to your history', 
+        	message: 'Records successfully added to your dashboard and history', 
         	records 
         })
     } catch (error) {
@@ -27,39 +37,78 @@ module.exports.createRecords = async (req, res) => {
 
 module.exports.getRecords = async (req, res) => {
 	try {
-		const records = await prisma.records.findMany()
+		const userId = req.user.id
+
+		const records = await prisma.records.findMany({
+			where: {
+				authorId: userId,
+				NOT: { status: "DELETED" }
+			},
+			orderBy: {
+				updatedAt: 'desc'
+			}
+		})
 
 		if(records.length === 0) {
-			return res.status(400).send({ message: 'No records found' })
+			return res.status(400).send({ error: 'No records found' })
 		}
 
 		return res.status(200).send(records)
 	} catch (err) {
-		return res.status(500).send({ message: 'Error getting records' })
+		return res.status(500).send({ error: 'Error getting records' })
 	}
 }
 
 module.exports.updateRecord = async (req, res) => {
 	try {
+		const { id, authorId, createdAt, ...safeBody } = req.body
         const { recordId } = req.params
         const userId = req.user.id
 
-        const updatedRecord = await prisma.records.update({
-            where: { 
-                id: Number(recordId),
-                authorId: userId
-            },
-            data: req.body,
-            select: { id: true }
+        const updatedRecord = {
+        	...req.body,
+        	birthDate: new Date(req.body.birthDate)
+        }
+
+        const currentRecord = await prisma.records.findUnique({
+            where: { id: Number(recordId) }
         })
 
-        return res.status(200).send({ 
-            message: 'Record updated successfully', 
-            updatedRecord 
-        })
+        if (!currentRecord || currentRecord.authorId !== userId) {
+            return res.status(404).send({ error: "Record not found" });
+        }
+
+        const isDifferent = Object.keys(updatedRecord).some(
+            key => updatedRecord[key].toString() !== currentRecord[key].toString()
+        );
+
+        if(!isDifferent) {
+            return res.status(200).send({ 
+                message: 'No changes detected', 
+                updatedRecord: { id: currentRecord.id } 
+            });
+        } else {
+        	const updatedRecord = await prisma.records.update({
+        	    where: { 
+        	        id: Number(recordId),
+        	        authorId: userId
+        	    },
+        	    data: {
+        	    	...safeBody,
+        	    	birthDate: new Date(req.body.birthDate),
+        	    	status: "UPDATED"
+        	    },
+        	    select: { id: true }
+        	})
+
+        	return res.status(200).send({ 
+        	    message: 'Record updated successfully', 
+        	    updatedRecord 
+        	})
+        }
     } catch (error) {
         console.error(error)
-        return res.status(500).send({ error: 'Failed to create record' })
+        return res.status(500).send({ error: 'Failed to update record' })
     }
 }
 
@@ -68,19 +117,20 @@ module.exports.deleteRecords = async (req, res) => {
 		const userId = req.user.id
 		const { ids } = req.body
 
-		const archivedRecord = await prisma.records.deleteMany({
+		const archivedRecord = await prisma.records.updateMany({
 			where: {
 				id: { in: ids.map(id => Number(id))}, 
 				authorId: userId
-			}
+			},
+			data: { status: "DELETED" }
 		})
 
 		return res.status(200).send({
 			message: 'Records deleted successfully',
-			archiveRecord
+			archivedRecord
 		})
 	} catch (error) {
 		console.log(error)
-		return res.status(500).send({ message: 'Failed to archived record'})
+		return res.status(500).send({ message: 'Failed to delete record'})
 	}
 }
