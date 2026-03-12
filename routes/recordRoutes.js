f021eabfdd25e08bc8564d382539dc90f40ee822
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-12 18:53:24 +0800

    Added params for update record

--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const { verify } = require('../auth')
 const recordController = require('../controllers/recordController.js')
 
-router.get('/get-records', recordController.getRecords)
+router.get('/get-records', verify, recordController.getRecords)
 router.post('/create-records', verify, recordController.createRecords)
-router.put('/update-records', verify, recordController.updateRecord)
-router.delete('/delete-records', verify, recordController.deleteRecords)
+router.put('/update-record/:recordId', verify, recordController.updateRecord)
+router.put('/delete-records', verify, recordController.deleteRecords)
 
 module.exports = router
