bfdf2747a0d542aeeb76572526a1785c7816d5e8
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-14 16:31:21 +0800

    Added get total records

--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -4,6 +4,7 @@ const { verify } = require('../auth')
 const recordController = require('../controllers/recordController.js')
 
 router.get('/get-records', verify, recordController.getRecords)
+router.get('/get-total-records', verify, recordController.getTotalRecords)
 router.post('/create-records', verify, recordController.createRecords)
 router.put('/update-record/:recordId', verify, recordController.updateRecord)
 router.put('/delete-records', verify, recordController.deleteRecords)
